730022c156fa2b2302dd33fb2d3c5b9e333dff85
Jacob Marquez <marcusfenixdubstep@gmail.com>
2019-03-18 16:27:04 -0700

    2019 update

--- app.js
+++ app.js
@@ -1,51 +1,93 @@
 var logger = (function () {
-
-    return {
-      start = function(){
-        // This works on all devices/browsers, and uses IndexedDBShim as a final fallback
-        var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-
-        // Open (or create) the database
-        var open = indexedDB.open("frlogger", 1);
-
-        // Create the schema
-        open.onupgradeneeded = function() {
-            var db = open.result;
-            var store = db.createObjectStore("logs", {keyPath: "id"});
-            var index = store.createIndex("Message", "time", "message");
-        };
-
-        open.onsuccess = function() {
-            // Start a new transaction
-            var db = open.result;
-            var tx = db.transaction("Message", "readwrite");
-            var store = tx.objectStore("Message");
-            var index = store.index("Message");
-
-            // Add some data
-            store.put({id: 1, time: Datetime.now, message: "this is a test"});
-            store.put({id: 2, time: Datetime.now, message: "this is a test"});
-
-            // Query the data
-            //var getJohn = store.get(12345);
-            //var getBob = index.get(["Smith", "Bob"]);
-
-            /*getJohn.onsuccess = function() {
-                console.log(getJohn.result.name.first);  // => "John"
-            };
-
-            getBob.onsuccess = function() {
-                console.log(getBob.result.name.first);   // => "Bob"
-            };*/
-
-            // Close the db when the transaction is done
-            tx.oncomplete = function() {
-                db.close();
-            };
+    if (window.localStorage){
+        var event = new CustomEvent('logFlush', { 
+            detail:{
+                logs: ()=>{ 
+                    let logs = window.localStorage.getItem("frLogger");
+                    window.localStorage.setItem("frLogger","");
+                    return logs;
+                } 
+            }
+        });
+        var count;
+        if (window.localStorage.getItem("frLogger")==undefined){
+            logStorage = window.localStorage.setItem("frLogger","");
+            count = 0;
+        } else {
+            count = window.localStorage.getItem("frLogger").split("~").length;
         }
-      },
-      add: function(){
-
-      }
+        function log(message,type){
+            let curr = window.localStorage.getItem("frLogger");
+            let toAdd = `${type}:${message}`;
+            if (count>0){ toAdd = `~`+toAdd; }
+            window.localStorage.setItem("frLogger",`${curr}${toAdd}`);
+            count++;
+            if (count > 10){
+                document.dispatchEvent(event);
+                count = 0;
+            }
+        }
+        console._log = (msg,...ar)=>{
+            if (ar){
+                for (var x = 0; x < ar.length; x++){ 
+                    if (Array.isArray(ar[x])){
+                        msg += ar[x].toString() 
+                    }
+                    else if (typeof ar[x] === "object"){
+                        msg += JSON.stringify(ar[x])
+                    } else {
+                        msg += ar[x];
+                    }
+                }
+            }
+            log(msg,"INFO");
+            console.log(msg);
+        }
+        console._warn = (msg,...ar)=>{
+            if (ar){
+                for (var x = 0; x < ar.length; x++){ 
+                    if (Array.isArray(ar[x])){
+                        msg += ar[x].toString() 
+                    }
+                    else if (typeof ar[x] === "object"){
+                        msg += JSON.stringify(ar[x])
+                    } else {
+                        msg += ar[x];
+                    }
+                }
+            }
+            log(msg,"WARN");
+            console.warn(msg);
+        }
+        console._error = (msg,...ar)=>{
+            if (ar){
+                for (var x = 0; x < ar.length; x++){ 
+                    if (Array.isArray(ar[x])){
+                        msg += ar[x].toString() 
+                    }
+                    else if (typeof ar[x] === "object"){
+                        msg += JSON.stringify(ar[x])
+                    } else {
+                        msg += ar[x];
+                    }
+                }
+            }
+            log(msg,"ERROR");
+            console.error(msg);
+        }
+        return {
+            read(_logs){
+                var logs = ``;
+                if (_logs==undefined && window.localStorage.getItem("frLogger")==""){
+                    return "no logs";
+                }
+                var storage = window.localStorage.getItem("frLogger").split("~").join("\n");
+                if (_logs){
+                    storage = _logs.split("~").join("\n");
+                }
+                var length = storage.split("\n").length;
+                return `${length} logs\n${storage}`;
+            }
+        }
     }
 })();
